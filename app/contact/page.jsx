0e2b5c5e11818e4d53d4664fe647a1660b5ce4c4
pr-ship-it <developer6969.tech@gmail.com>
2025-04-28 10:16:12 -0600

    Add files via upload

---
+++ app/contact/page.jsx
@@ -0,0 +1,14 @@
+import { Globe } from "lucide-react"
+import ServiceDetail from "@/components/service-detail"
+import ContactSection from "@/components/contact-section"
+import WebDesignSimulator from "@/components/web-design-simulator"
+import Navbar from "@/components/navbar"
+
+export default function DisenoWebPage() {
+  return (
+    <>
+     <Navbar />
+      <ContactSection />
+    </>
+  )
+}
